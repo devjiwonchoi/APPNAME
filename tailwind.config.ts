869b53be96fc80c98fc219031b2c75cf5204a0b5
Jiwon Choi <devjiwonchoi@gmail.com>
2024-05-19 14:36:48 +0900

    style: layout setup (#4)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,5 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  content: ['./src/{app,components}/**/*.{tsx,mdx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['var(--font-geist-sans)'],
-        mono: ['var(--font-geist-mono)'],
-      },
-    },
-  },
-  plugins: [],
+  content: ['./src/{app,components}/**/*.tsx'],
 } satisfies Config
